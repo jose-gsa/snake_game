016c2f69e8cd53f02abf0340c32d59a8c4202d73
jose-gsa <jose_16211@proton.me>
2026-09-04 18:43:35 -0400

    feat: Snake data structure

--- include/snake.h
+++ include/snake.h
@@ -3,6 +3,19 @@
 
 class Snake {
 
+  typedef struct Node {
+
+    int pos_x;
+    int pos_y;
+    struct Node *prev;
+    struct Node *next;
+
+  } Node;
+
+  Node *head; // start of list
+  Node *tail; // end of list
+  unsigned size;
+
 public:
   Snake() = default;
   ~Snake() = default;
