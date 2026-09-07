27131064c6fde61bef9d889a17335f8bc1328085
jose-gsa <jose_16211@proton.me>
2026-09-07 13:27:10 -0400

    feat: Crate game class

---
+++ include/game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "snake.h"
+
+class Game {
+
+  int map_width_X;
+  int map_height_Y;
+  Snake snake;
+  Position apple;
+  std::vector<Position> snakePositions;
+
+  void spawnApple();
+
+public:
+  Game(int x, int y);
+  ~Game() = default;
+  void update();
+  void processInput(Direction dir);
+};
+
+#endif // !GAME_H
