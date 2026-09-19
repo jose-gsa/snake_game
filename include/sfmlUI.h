ce4fd1d49e52aad0ac206b738bad80d59f42bbaa
jose-gsa <jose_16211@proton.me>
2026-09-18 20:06:05 -0400

    assets: Create new atributes for all assets

--- include/sfmlUI.h
+++ include/sfmlUI.h
@@ -4,11 +4,29 @@
 #include "game.h"
 #include "snake.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
 
 class SfmlUI {
 
   sf::RenderWindow window;
   int tileSize;
+
+  sf::Texture textureApple;
+  sf::Texture textureSnakeHead;
+  sf::Texture textureSnakeBody;
+  sf::Texture textureSnakeTurn;
+  sf::Texture textureSnakeTail;
+  sf::Texture textureGrassLight;
+  sf::Texture textureGrassDark;
+
+  sf::Sprite spriteApple;
+  sf::Sprite spriteSnakeHead;
+  sf::Sprite spriteSnakeBody;
+  sf::Sprite spriteSnakeTurn;
+  sf::Sprite spriteSnakeTail;
+  sf::Sprite spriteGrassLight;
+  sf::Sprite spriteGrassDark;
 
 public:
   SfmlUI(int mapWidth, int mapHeight, int tileSize);
